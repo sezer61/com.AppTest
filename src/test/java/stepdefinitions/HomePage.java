0aa548e15e7999ea65006e17e10d136c6c169cc5
hakankob0@gmail.com <sezer.0734@hotmail.com>
2023-02-28 23:41:56 +0300

    upd

---
+++ src/test/java/stepdefinitions/HomePage.java
@@ -0,0 +1,39 @@
+package stepdefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import pages.Allpages;
+import utils.ConfigReader;
+import utils.Driver;
+
+public class HomePage {
+     Allpages allpages=new Allpages();
+
+
+     @Given("Alltricks anasayfasina git")
+     public void alltricks_anasayfasina_git() {
+          Driver.getAppiumDriver().get(ConfigReader.getProperty("Url"));
+          allpages.cookie.click();
+          allpages.singIn.click();
+     }
+
+     @Given("Profil sekmesine geç")
+     public void profil_sekmesine_gec() {
+
+     }
+
+     @Then("Adresim kismina emaili gir")
+     public void adresim_kismina_emaili_gir() {
+
+
+     }
+
+
+
+
+
+
+
+}
---
+++ src/test/java/utils/ConfigReader.java
@@ -0,0 +1,22 @@
+package utils;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.util.Properties;
+public class ConfigReader {
+    static Properties properties;
+
+    static {
+        String path="configuration.properties";
+        try {
+            FileInputStream fileInputStream=new FileInputStream(path);
+            properties=new Properties();
+            properties.load(fileInputStream);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    public static String getProperty(String key){
+        return properties.getProperty(key);
+    }
+}
