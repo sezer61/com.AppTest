c70fa44bd3b21684808075cb24eaacc2a44edf0b
Hakan Sezer <109696525+sezer61@users.noreply.github.com>
2023-02-28 23:44:49 +0300

    Update Hooks.java

--- src/test/java/stepdefinitions/Hooks.java
+++ src/test/java/stepdefinitions/Hooks.java
@@ -4,7 +4,7 @@ import io.cucumber.java.After;
 import io.cucumber.java.Scenario;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
-import utilities.Driver;
+import utils.Driver;
 import utils.Driver;
 
 public class Hooks {
